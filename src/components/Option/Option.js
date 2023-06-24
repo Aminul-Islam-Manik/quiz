39a6b02e11f6fd28e14de0157969d6c4b72d9c54
Tapushi Nilu <mntapushi123@gmail.com>
2023-06-24 10:11:31 -0500

    dynamecally quiz question setup

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -2,10 +2,9 @@ import React from 'react';
 import './Option.css'
 
 const Option = ({option}) => {
-    console.log(option)
+    // console.log(option)
     return (
         <div className='option'>
-            {/* <button type='redio'>{option}</button> */}
             <input type="radio" /><span><p>{option}</p></span>
         </div>
     );
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Questions from '../Questions/Questions';
 import './Quiz.css';
+import Option from '../Option/Option';
 
 const Quiz = () => {
     const quizes = useLoaderData();
-    console.log(quizes.data)
-    // const {logo} = quizes.data;
-    // console.log(question)
+    // console.log(quizes.data)
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(1);
+
+    const handleAnswerOptionClick = () => {
+        const newScore = score + 1;
+        setScore(newScore);
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < allQuizes.length) {
+			setCurrentQuestion(nextQuestion);
+		} 
+	};
+    const allQuizes = quizes.data.questions;
+
+    // console.log(allQuizes)
+
     return (
         <div className='quiz-wrapper'>
             <div className="quiz-img">
                 <img src={quizes.data.logo} alt="" />
             </div>
             <div className="quiz">
-                <h3>All quiz here:{quizes.data.questions.length}</h3>
+                
+                <h4>Your quiz{score}/{quizes.data.questions.length}</h4>
+                {allQuizes[currentQuestion].question}
+
                 {
-                quizes.data.questions.map(qs=><Questions
-                    key={qs.id}
-                    qs={qs}
-                ></Questions>)  
+                    allQuizes[currentQuestion].options.map(option=><Option
+                    key={option.idx}
+                    option={option}
+                    ></Option>)
                 }
+
+                <button className="btn" onClick={()=>handleAnswerOptionClick()}>Next</button>
             </div>
         </div>
     );
